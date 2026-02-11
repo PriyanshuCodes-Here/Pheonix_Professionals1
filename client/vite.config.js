cd493ffbc59a6a15c26c820d29e83acf52e4cf3a
Priyanshu Bisht <priyanshubisht7924@gmail.com>
2026-02-11 22:52:35 +0530

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       external: ['fsevents']
